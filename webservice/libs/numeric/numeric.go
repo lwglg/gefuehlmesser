7692306801dc707810158f0f582827f3b6608014
Guilherme Lima Gonçalves <lwglguilherme@gmail.com>
2025-10-08 01:14:07 -0300

    feat: implement numerical lib

---
+++ webservice/libs/numeric/numeric.go
@@ -0,0 +1,61 @@
+package numeric
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+// Both functions adapted from: https://stackoverflow.com/a/77009270/29040254
+
+// Precision controls the number of digits (excluding the exponent)
+// Precision of -1 uses the smallest number of digits
+func TruncFloat(f float64, precision int) (float64, error) {
+	floatBits := 64
+
+	if math.IsNaN(f) || math.IsInf(f, 1) || math.IsInf(f, -1) {
+		return 0, fmt.Errorf("bad float val %f", f)
+	}
+
+	fTruncStr := strconv.FormatFloat(f, 'f', precision+1, floatBits)
+	fTruncStr = fTruncStr[:len(fTruncStr)-1]
+	fTrunc, err := strconv.ParseFloat(fTruncStr, floatBits)
+	if err != nil {
+		return 0, err
+	}
+
+	return fTrunc, nil
+}
+
+// Precision controls the number of digits (excluding the exponent)
+// Precision of -1 uses the smallest number of digits
+func RoundFloat(f float64, precision int) (float64, error) {
+	floatBits := 64
+
+	if math.IsNaN(f) || math.IsInf(f, 1) || math.IsInf(f, -1) {
+		return 0, fmt.Errorf("bad float val %f", f)
+	}
+
+	fRoundedStr := strconv.FormatFloat(f, 'f', precision, floatBits)
+	fRounded, err := strconv.ParseFloat(fRoundedStr, floatBits)
+	if err != nil {
+		return 0, err
+	}
+
+	return fRounded, nil
+}
+
+func RoundFloatFast(f float64, precision int) (float64, error) {
+	mul := math.Pow10(precision)
+	if mul == 0 {
+		return 0, nil
+	}
+
+	product := f * mul
+	var roundingErr error
+	if product > float64(math.MaxInt64) {
+		roundingErr = fmt.Errorf("unsafe round: float64=%+v, places=%d", f, precision)
+	}
+
+	return math.Round(product) / mul, roundingErr
+}
---
+++ webservice/libs/numeric/numeric_test.go
@@ -0,0 +1,86 @@
+package numeric_test
+
+import (
+	"testing"
+	"time"
+
+	n "webservice/libs/numeric"
+)
+
+func TestRoundFloatFast(t *testing.T) {
+	a := time.Now().UnixMilli()
+	for i := 0.; i < 1_000_000; i += 1.23456789 {
+		n.RoundFloatFast(i, 2)
+	}
+	t.Logf("RoundFloatFast millis=%d", time.Now().UnixMilli()-a)
+}
+
+func TestRoundFloat(t *testing.T) {
+	f, _ := n.RoundFloat(0.000000000000000000000000000000011, 32)
+	expected := 0.00000000000000000000000000000001
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	f, _ = n.RoundFloat(92234, 14)
+	expected = 92234.
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	f, _ = n.RoundFloat(2.289, 2)
+	expected = 2.29
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	f, _ = n.RoundFloat(111222333444555666777888999000111222333.123456789, 8)
+	expected = 111222333444555666777888999000111222333.12345679
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	a := time.Now().UnixMilli()
+	for i := 0.; i < 1_000_000; i += 1.23456789 {
+		n.RoundFloat(i, 2)
+	}
+	t.Logf("RoundFloat millis=%d", time.Now().UnixMilli()-a)
+}
+
+func TestTruncFloat(t *testing.T) {
+	f, _ := n.TruncFloat(0.000000000000000000000000000000019, 32)
+	expected := 0.00000000000000000000000000000001
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	f, _ = n.TruncFloat(2.289, 2)
+	expected = 2.28
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	f, _ = n.TruncFloat(111222333444555666777888999000111222333.123456789, 8)
+	expected = 111222333444555666777888999000111222333.12345678
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	f, _ = n.TruncFloat(1, 0)
+	expected = 1.
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	f, _ = n.TruncFloat(1, 2)
+	expected = 1.
+	if f != expected {
+		t.Errorf("Expected=%+v Actual=%+v", expected, f)
+	}
+
+	a := time.Now().UnixMilli()
+	for i := 0.; i < 1_000_000; i += 1.23456789 {
+		n.TruncFloat(i, 2)
+	}
+	t.Logf("TruncFloat millis=%d", time.Now().UnixMilli()-a)
+}
